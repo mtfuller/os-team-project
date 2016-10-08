ba6a9a5c1919fdd635bc6bea9a5ab051502bf493
MargNation <margaret.w.harriman@gmail.com>
2016-10-08 14:06:44 -0400

    Added sorting methods, Kernel is complete

--- src/com/kennesaw/OS_Module/Kernel.java
+++ src/com/kennesaw/OS_Module/Kernel.java
@@ -11,38 +11,53 @@ import java.util.ArrayList;
 public class Kernel {
     
     ArrayList<PCB> pcbQueue;
-    ArrayList<PCB> sortedReadyQueue;
     
     public Kernel() {
         pcbQueue = new ArrayList<>();
-        sortedReadyQueue = new ArrayList<>();
     }
     
-    //    public void sortPCBs(String algo) {
-//        ArrayList<PCB> pcbsBefore = new ArrayList<>();
-//        for (int i = 0; i < 10; i++) {
-//            Random ran = new Random();
-//            int x = ran.nextInt(6) + 5;
-//            pcbsBefore.add(new PCB(i + 1, (x + 20), x, (x + 555)));
-//            System.out.println(pcbsBefore.get(i).getJobID());
-//            System.out.println(pcbsBefore.get(i).getPriority());
-//        }
-//    }
+    public void sortPriority() {
+        for (int j = 1; j < pcbQueue.size(); j++) {
+            for (int q = 0; q < pcbQueue.size(); q++) {
+            }
+            PCB key = pcbQueue.get(j);
+            int i = j - 1;
+            while ((i > -1) && (pcbQueue.get(i).getPriority() > key.getPriority())) {
+                pcbQueue.set((i + 1), pcbQueue.get(i));
+                i--;
+            }
+            pcbQueue.set((i + 1), key);
+        }
+    }
+    
+    public void sortSJF() {
+        for (int j = 1; j < pcbQueue.size(); j++) {
+            for (int q = 0; q < pcbQueue.size(); q++) {
+            }
+            PCB key = pcbQueue.get(j);
+            int i = j - 1;
+            while ((i > -1) && (pcbQueue.get(i).getJobSize() > key.getJobSize())) {
+                pcbQueue.set((i + 1), pcbQueue.get(i));
+                i--;
+            }
+            pcbQueue.set((i + 1), key);
+        }
+    }
     
     public PCB getPCB(int index) {
-        return sortedReadyQueue.get(index);
+        return pcbQueue.get(index);
     }
     
     public void addPCB(int index, PCB newPCB) {
-        sortedReadyQueue.add(index, newPCB);
+        pcbQueue.add(index, newPCB);
     }
     
     public int getQueueSize() {
-        return sortedReadyQueue.size();
+        return pcbQueue.size();
     }
 
     public ArrayList<PCB> accessReadyQueue() {
-        return sortedReadyQueue;
+        return pcbQueue;
     }
     
 }
