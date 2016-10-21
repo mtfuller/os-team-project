6b51ff9fc20fface271bdf5cbdf75e9bf7a8c875
Margaret Harriman <margaret.w.harriman@gmail.com>
2016-10-21 17:17:59 -0400

    Add files via upload

--- src/com/kennesaw/Analyzer/Analysis.java
+++ src/com/kennesaw/Analyzer/Analysis.java
@@ -1,5 +1,6 @@
 package com.kennesaw.Analyzer;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 /**
@@ -14,31 +15,34 @@ public class Analysis {
     private static ArrayList<Double> cpuSpace = new ArrayList<>();
     private static ArrayList<Integer> cpuID1= new ArrayList<>();
     private static ArrayList<Double> ramSpace1 = new ArrayList<>();
-
+    
     public static void initializeDataTables() {
         for (int i = 0; i < 30; i++) {
+            createTimes.add(0L);
+            waitTimes.add(0L);
+            completeTimes.add(0L);
             io.add(0);
             cpuSpace.add(0.0);
             cpuID1.add(0);
             ramSpace1.add(0.0);
         }
     }
-
+    
     public static void recordNumOFJobs(int jobID){
-        jobID1.add(jobID,jobID);
+        jobID1.add(jobID,jobID + 1);
     }
     public static void recordCreateTime(int jobID){
-        createTimes.add(jobID, System.nanoTime());
-
+        createTimes.set(jobID, System.nanoTime());
+        
     }
     public static void recordWaitTime(int jobID){
-        waitTimes.add(jobID,System.nanoTime());
-
-
+        waitTimes.set(jobID,System.nanoTime());
+        
+        
     }
     public static void recordCompleteTime(int jobID){
-        completeTimes.add(jobID, System.nanoTime());
-
+        completeTimes.set(jobID, System.nanoTime());
+        
     }
     public static void recordIO(int jobID, int numOFIO){
         io.set(jobID, numOFIO);
@@ -50,35 +54,38 @@ public class Analysis {
     public static void recordRamSpace(int jobID, double ramSpace){
         ramSpace1.add(jobID, ramSpace);
     }
-
-
+    
+    
     public static void calctoString() {
-
-
+        
+        
         //subtracts createTime by waitTime
-
+        
         ArrayList<Long> realWait = new ArrayList<>();
-
+        ArrayList<Long> realComplete = new ArrayList<>();
         for(int i = 0; i < 30; i++){
             realWait.add(waitTimes.get(i) - createTimes.get(i));
+            realComplete.add(completeTimes.get(i) - waitTimes.get(i));
         }
-
+        
         //printout into table format all arraylists must have same size to work
 
-//        System.out.println(String.format("%5s %15s %15s %15s %15s %15s %15s", "JobID", "Create Times", "End Wait Times, Wait Times",
-//               "Complete Times", "# of IO", "CPUID", "CPU Space", "RAM Space"));
+        System.out.println(String.format("%5s %15s %15s %15s %15s %15s %15s", "JobID", "Wait Times",
+               "Complete Times", "# of IO", "CPUID", "CPU Space", "RAM Space"));
         for(int i = 0; i < 30; i ++) {
-            System.out.println(String.format("%5s %15s %15s %15s %15s", jobID1.get(i), createTimes.get(i), waitTimes.get(i),
-                    realWait.get(i), completeTimes.get(i)));
+            DecimalFormat df = new DecimalFormat("#.##");
+            String real_wait = df.format(realWait.get(i)/1000000.00);
+            String real_complete = df.format(realComplete.get(i)/1000000.00);
+            System.out.println(String.format("%5s    | %9s ms    | %15s", jobID1.get(i), real_wait, realComplete.get(i)));
             //io.get(i)));
             //cpuID1.get(i), cpuSpace.get(i), ramSpace1.get(i)));
-
+            
         }
-
-
-
+        
+        
+        
         //calc avg wait
-
+        
         long totalWait =0;
         int size = realWait.size();
         long avgWait = 0;
@@ -87,9 +94,9 @@ public class Analysis {
         }
         avgWait = totalWait/size;
         System.out.println("Average Wait Time: " + avgWait);
-
+        
         //calc avg complete
-
+        
         long totalComplete = 0;
         int size1 = completeTimes.size();
         long avgComplete = 0;
@@ -98,7 +105,7 @@ public class Analysis {
         }
         avgComplete = totalComplete/size1;
         System.out.println("Average Complete Time: " + avgComplete);
-
+        
     }
-
+    
 }
