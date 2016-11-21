c1e6421c8f2b15b385cbdfdd312bf44a34bdfbb6
Margaret Harriman <margaret.w.harriman@gmail.com>
2016-11-21 17:36:56 -0500

    Working on STS

--- src/com/kennesaw/OS_Module/ShortTermScheduler.java
+++ src/com/kennesaw/OS_Module/ShortTermScheduler.java
@@ -9,9 +9,11 @@ import java.util.ArrayList;
 import com.kennesaw.Analyzer.Analysis;
 import com.kennesaw.cpumodule.CPU;
 import com.kennesaw.cpumodule.DmaChannel;
+import com.kennesaw.cpumodule.MMU;
 import memory.Ram;
 
 public class ShortTermScheduler {
+    MMU simMMU;
     Ram simRAM;
     Kernel simKernel;
     ArrayList<CPU> cpuBank = new ArrayList<>();
@@ -20,15 +22,17 @@ public class ShortTermScheduler {
     public ShortTermScheduler(Ram ram, Kernel kernel, int numCPUs) {
         simRAM = ram;
         simKernel = kernel;
-//        for (int i = 0; i < numCPUs; i++) {
-//            cpuBank.add(new CPU(new DmaChannel(simRAM), i));
-//            cpuBank.get(i).start();
-//        }
-        for(int i = 0; i < 30; i++) {
-            double used = (double)simKernel.getPCB(i).getJobSize() / 1024.00;
-            Analysis.recordRamSpace(i, used);
+        simMMU = new MMU(simKernel,simRAM);
+        for (int i = 0; i < numCPUs; i++) {
+            cpuBank.add(new CPU(i, simMMU));
+            cpuBank.get(i).start();
         }
+//        for(int i = 0; i < 30; i++) {
+//            double used = (double)simKernel.getPCB(i).getJobSize() / 1024.00;
+//            Analysis.recordRamSpace(i, used);
+//        }
     }
+    
     
     public int findCPU() {
         boolean openCpuFound = false;
@@ -51,18 +55,19 @@ public class ShortTermScheduler {
         // Continue to spin as long as the Kernel has ready jobs
         while (simKernel.getPCB(simKernel.pcbQueue.size()-1).getStatus() != "Ended") {
             PCB nextJob = simKernel.getNextPCB();
+            System.out.println(nextJob.getStatus());
             cpuIndex = findCPU();
             while (cpuBank.get(cpuIndex).isRunningProcess());
-            if (nextJob.getStatus() == "Waiting") {
-                Analysis.recordWaitTime(nextJob.getJobID()-1);
+            if (nextJob.getStatus() == "Ready") {
+//                Analysis.recordWaitTime(nextJob.getJobID()-1);
                 cpuBank.get(cpuIndex).runPCB(nextJob);
                 simKernel.getPCB(simKernel.pcbQueuePointer).setStatus(4);
             }
         }
-        
+    
         // Resets the number of jobs in RAM
         simRAM.resetJobCount();
-        
+    
         // Spin until all cpus have finished processing
         for (CPU cpu : cpuBank) {
             while (cpu.isRunningProcess());
